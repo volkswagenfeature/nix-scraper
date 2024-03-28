4dd1437081dee4cdcb434d035e4090d8f007ef0b
volkswagenfeature <2@2.2>
2024-03-27 20:33:15 -0400

    Initial exploration

---
+++ README.md
@@ -0,0 +1,5 @@
+Quick usage instructions:
+- `nix run mkSymlinks` fetches the static js libs I'm planning on using.
+- `nix run` runs the Jupyter enviroment.
+
+At this time the pyproject.toml file does nothing.
---
+++ src/libs/.ipynb_checkpoints/fakewait-checkpoint.py
@@ -0,0 +1,18 @@
+import asyncio
+import numpy
+from time import sleep
+import base64
+import codecs
+
+class fakewaiter:
+    def __init__(self):
+        self.sleeptime = numpy.random.normal(3,2)
+        self.idval = codecs.decode(base64.b85encode(id(self).to_bytes(8,byteorder='big')))
+    
+    def info(self):
+        
+        return f"id ({self.idval}) will sleep {self.sleeptime:.3f} sec"
+        
+    def run(self):
+        sleep(self.sleeptime)
+        return f"id ({self.idval}) slept {self.sleeptime:.3f} sec"
